257f3c8f6394d82cbf0bb27d486df153b9353570
Jean Vinicius <118926021+Jean-V1nicius@users.noreply.github.com>
2022-12-29 22:02:10 -0300

    Fiz a versão desktop
    Fiz versão desktop exercício 26 Media Query 5

--- exercicios/ex026/script05.js
+++ exercicios/ex026/script05.js
@@ -31,17 +31,18 @@ function exbirMenu() {
     backgroundMenu.style.display = "block"
 
     // Colocar margem lateral nos itens do menu
-    if (menu.offsetWidth <= 296) {
-        for (var i = 0; i < 5; i++) {
-            linksMenu[i].style.margin = '8px'
-        }
-    } else {
-        // Voltar para o valor padrão *Que eu definido nas css
-        for (var i = 0; i < 5; i++) {
-            linksMenu[i].style.margin = '8px auto'
+    if (innerWidth < 769) {
+        if (menu.offsetWidth <= 296) {
+            for (var i = 0; i < 5; i++) {
+                linksMenu[i].style.margin = '8px'
+            }
+        } else {
+            // Voltar para o valor padrão *Que eu definido nas css
+            for (var i = 0; i < 5; i++) {
+                linksMenu[i].style.margin = '8px auto'
+            }
         }
     }
-
     // Esconder o título do site, inserir o título no menu e lihna horizontal
     tituloSite.style.visibility = "hidden"
     menu.insertBefore(tituloMenu, ulMenu)
@@ -49,23 +50,7 @@ function exbirMenu() {
 }
 
 function resizeView() {
-    if (innerWidth >= 769) {
-        menu.style.display = "block"
-        backgroundMenu.style.display = "none"
-
-        tituloSite.style.visibility = 'visible'
-        
-        // Remover o h1 e linha horizontal do menu quando sair do modo mobile
-        if (menu.childElementCount >= 3) {
-            menu.removeChild(tituloMenu)
-            menu.removeChild(linha)
-        }
-    }
-    // Evitar que o menu abra sozinho quando a tela for diminuida
-    if (innerWidth < 769 && backgroundMenu.style.display == 'none') {
-        menu.style.display = 'none'
-    }
-
+    // Configurações do modo mobile
     if (innerWidth < 769) {
         // Colocar margem lateral nos itens do menu 
         if (menu.offsetWidth <= 296) {
@@ -86,5 +71,47 @@ function resizeView() {
             // Voltar para o valor padrão *Que eu definido nas css
             menu.style.width = '55vw'
         }
+        // Remover os estilos do link do desktop
+        linksMenu[atual].style = ''
+    } else {
+        // Configurações do modo desktop
+        menu.style.display = "block"
+        backgroundMenu.style.display = "none"
+
+        tituloSite.style.visibility = 'visible'
+        
+        // Remover o h1 e linha horizontal do menu quando sair do modo mobile
+        if (menu.childElementCount >= 3) {
+            menu.removeChild(tituloMenu)
+            menu.removeChild(linha)
+        }
+        
+        // Remover a margem dos links do menu para o modo desktop
+        for (i = 0; i < 5; i++) {
+            linksMenu[i].style.margin = '0px'
+        }
+
+        // Readicionar borda para o link selecionado anteriormente
+        linksMenu[atual].style = 'font-weight: bold; border-bottom: 4px solid var(--cor04);'
+    }
+
+    // Evitar que o menu abra sozinho quando a tela for diminuida
+    if (innerWidth < 769 && backgroundMenu.style.display == 'none') {
+        menu.style.display = 'none'
+    }
+}
+
+var atual = 0
+// Adicionar borda ao primeiro link
+linksMenu[atual].style = 'font-weight: bold; border-bottom: 4px solid var(--cor04);'
+
+function selecionar(posicao) {
+    if (innerWidth > 768) {
+        // Remover estilos do link atual
+        linksMenu[atual].style = ''
+        // Adicionar estilos ao novo link selecionado
+        linksMenu[posicao].style = 'font-weight: bold; border-bottom: 4px solid var(--cor04);'
+        // Adicionar a posição do link selecionado a variável atual
+        atual = posicao
     }
 }
